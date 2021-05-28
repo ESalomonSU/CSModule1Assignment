f923746b4af3868b321d0285b63cb7e70cfe358e
Esteban Salomon <esalomon6492@southwestuniversity.edu>
2021-05-28 02:23:17 -0600

    CS Module 1 Assignment

---
+++ Module1Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Module1Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myName();
+            Console.ReadKey();
+        }
+
+        public static void myName()
+        {
+            Console.WriteLine("My name is Esteban");
+        }
+    }
+}
+
+    
